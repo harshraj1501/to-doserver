b84370685167fd5d08b1c56c50f95fa3ee1a4dce
Harsh <hkmishra021@gmail.com>
2024-07-22 00:00:04 +0530

    server commit

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ import bodyParser from 'body-parser';
 
 import Connection from './database/db.js';
 import Routes from './routes/route.js';
-import path from 'path';
 
-const __dirname = path.resolve();
+
 
 
 const app = express();
@@ -19,11 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', Routes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+
 
 const PORT = 8000;
 
